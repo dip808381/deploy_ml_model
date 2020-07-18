f29ec136b6f739977a1a825de45cb1f9722f4cac
Dipesh Barai <dipeshstat05@gmail.com>
2020-07-18 06:45:53 +0530

    ml_model

---
+++ flaskapp.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+from reg import classifier
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def classifierA():
+    if request.method == "POST":
+        val = int(request.form.get("nm"))
+        out = classifier(val)
+        return render_template("login.html",out="The Predicted Salary Is:"+ str(out[0]))
+
+    return render_template("login.html",out="")
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port = 8080)
+
+if __name__ == "__main__":
+    app.run(debug=True)
